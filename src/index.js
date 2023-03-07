ebe15b4e42eaae8a7b690783fd146e8d6ba97004
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-07 12:46:46 +0000

    add index.js and js modules file for show user name and score

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import populate from './module/populate';
+import store from './module/store';
+
+store.forEach((element) => {
+  populate(element.userName, element.score, element.index)
+});
