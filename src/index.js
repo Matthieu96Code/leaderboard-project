653caa4be58c3684d553129290f0deeb7828e51f
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-08 13:34:02 +0000

    fix refresh button select

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import populate from './module/populate';
 import store from './module/store';
 import getList from './module/getlist';
 
-document.getElementById('submit-btn').addEventListener('click', () => {
+document.getElementById('refresh-btn').addEventListener('click', () => {
   getList().then((list) => {
     list.result.forEach((element) => {
       populate(element.user, element.score);
