965aa0f33257bee814a76d9531a2a017214fcab7
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-08 13:34:45 +0000

    delete the store mock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 import populate from './module/populate';
-import store from './module/store';
 import getList from './module/getlist';
 
 document.getElementById('refresh-btn').addEventListener('click', () => {
