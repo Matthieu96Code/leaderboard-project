4c05b6d52f8ebcc7fe23228910dfaef207fc4cdd
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-08 14:24:27 +0000

    split refresh event

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import populate from './module/populate';
 import getList from './module/getlist';
 import sendList from './module/sendlist';
 
-document.getElementById('refresh-btn').addEventListener('click', () => {
+const callList = () => {
+  document.getElementById('scores-ul').innerHTML = '';
   getList().then((list) => {
     list.result.forEach((element) => {
-      document.getElementById('scores-ul').innerHTML = '';
       populate(element.user, element.score);
     });
   });
+}
+
+document.getElementById('refresh-btn').addEventListener('click', () => {
+  callList()
 });
+
