130aeb7c9e3b14cdc390d818702266963496df6c
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-08 13:45:30 +0000

    fix the refresh event by cleaning the content before

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import sendList from './module/sendlist';
 document.getElementById('refresh-btn').addEventListener('click', () => {
   getList().then((list) => {
     list.result.forEach((element) => {
+      document.getElementById('scores-ul').innerHTML = '';
       populate(element.user, element.score);
     });
   });
